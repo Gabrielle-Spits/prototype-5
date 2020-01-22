99606f0e2b8098c44f05df7b955a6ceb113efdda
Gabrielle Spits <gabrielle-spits@hotmail.com>
2020-01-22 12:25:03 +0100

    no message

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody tartgetRb;
+    private GameManager gameManager; 
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorgue = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -2;
+
+    public ParticleSystem explosionParticle;
+    public int pointValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tartgetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        
+        tartgetRb.AddForce(RandomForce(),ForceMode.Impulse);
+        tartgetRb.AddTorque(RandomTorgue(), RandomTorgue(), RandomTorgue(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnpos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorgue()
+    {
+        return Random.Range(-maxTorgue, maxTorgue);
+    }
+
+    Vector3 RandomSpawnpos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
